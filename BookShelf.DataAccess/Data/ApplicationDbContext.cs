2617dbfbb3d1179de9f7857e6d1caad3ea973fcf
foram <foramshah0803@gmail.com>
2024-06-14 19:48:21 +0530

    Scaffold Identity Issue

--- BookShelf.DataAccess/Data/ApplicationDbContext.cs
+++ BookShelf.DataAccess/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
 ﻿
 using BookShelf.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace BookShelf.DataAccess.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         
@@ -17,6 +18,8 @@ namespace BookShelf.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<category>().HasData(
                 new category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new category { Id = 2, Name = "SciFic", DisplayOrder = 2 },
