69342c1a9622a3d41ae64ec3d54e702cc02a38d7
foram <foramshah0803@gmail.com>
2024-06-16 16:41:21 +0530

    Add Identity tables

--- BookShelf.web/Program.cs
+++ BookShelf.web/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>
     (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultString")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
@@ -32,6 +33,9 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
+
 
 app.MapControllerRoute(
     name: "default",
