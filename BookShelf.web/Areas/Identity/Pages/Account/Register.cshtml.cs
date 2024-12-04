ca4a715495b86a6d927cca77bb518d9f3d6da60b
foram <foramshah0803@gmail.com>
2024-12-04 17:42:00 +0530

    Display Company Dropdown

--- BookShelf.web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookShelf.web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using BookShelf.DataAccess.Repository.IRepository;
 using BookShelf.Models;
 using BookShelf.Utility;
 using Microsoft.AspNetCore.Authentication;
@@ -35,7 +36,7 @@ namespace BookShelf.web.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
-
+        private readonly IUnitOfWork _unitOfWork;
         public RegisterModel(
 
             UserManager<IdentityUser> userManager,
@@ -43,8 +44,10 @@ namespace BookShelf.web.Areas.Identity.Pages.Account
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _roleManager = roleManager;
             _userManager = userManager;
             _userStore = userStore;
@@ -120,6 +123,9 @@ namespace BookShelf.web.Areas.Identity.Pages.Account
             public string? PostalCode { get; set; }
             public string? PhoneNumber { get; set; }
 
+            public int? CompanyId {  get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
         }
 
 
@@ -141,9 +147,16 @@ namespace BookShelf.web.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
-                })
+                }),
 
-            };
+				CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				})
+
+
+			};
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
