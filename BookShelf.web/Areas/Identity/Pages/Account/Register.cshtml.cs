bd929512c352979b65d770d8b5bb52d2dbccc106
foram <foramshah0803@gmail.com>
2024-12-04 23:49:57 +0530

    Register Company User

--- BookShelf.web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookShelf.web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -176,6 +176,11 @@ namespace BookShelf.web.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if(Input.Role == SD.Role_Company){
+                    user.CompanyId=Input.CompanyId;
+
+                }
                 user.Name = Input.Name;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
