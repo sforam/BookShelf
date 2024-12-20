858110fc342f31a02b6501de9824e21af3d2843e
foram <foramshah0803@gmail.com>
2024-12-20 15:36:34 +0530

    Creating Admin and Employee Accounts

--- BookShelf.web/Areas/Customer/Controllers/CartController.cs
+++ BookShelf.web/Areas/Customer/Controllers/CartController.cs
@@ -181,6 +181,7 @@ namespace BookShelf.web.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStaus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
                 }
+                HttpContext.Session.Clear();
             }
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
